cb88674a4ad338fee544d7738634ba6f55a924a4
mmkal <15040698+mmkal@users.noreply.github.com>
2019-06-16 03:05:24 -0400

    cli migrator

---
+++ packages/migrator/src/index.ts
@@ -0,0 +1,119 @@
+import {createHash} from 'crypto'
+import {existsSync, readFileSync, writeFileSync, mkdirSync} from 'fs'
+import {memoize, once} from 'lodash'
+import {basename, dirname, join} from 'path'
+import * as Umzug from 'umzug'
+import {sql, DatabasePoolType} from 'slonik'
+
+export interface SlonikMigratorOptions {
+  slonik: DatabasePoolType
+  migrationsPath?: string
+  migrationTableName?: string
+  log?: typeof console.log
+  args?: string[]
+  mainModule?: NodeModule
+}
+
+export interface Migration {
+  file: string
+  path: string
+}
+
+export interface SlonikMigrator {
+  up(migration?: string): Promise<Migration[]>
+  down(migration?: string): Promise<Migration[]>
+  create(migration: string): void
+}
+
+export const setupSlonikMigrator = ({
+  slonik,
+  migrationsPath = join(__dirname, 'migrations'),
+  migrationTableName = 'migration',
+  log = console.log,
+  args = process.argv.slice(2),
+  mainModule,
+}: SlonikMigratorOptions) => {
+  log = memoize(log, JSON.stringify)
+  const createMigrationTable = once(async () => {
+    void await slonik.query(sql`
+      create table if not exists ${sql.identifier([migrationTableName])}(
+        name text primary key,
+        hash text not null,
+        date timestamptz not null default now()
+      )
+    `)
+  })
+  const hash = (migrationName: string) => createHash('md5')
+    .update(readFileSync(join(migrationsPath, migrationName), 'utf8').trim().replace(/\s+/g, ' '))
+    .digest('hex')
+    .slice(0, 10)
+  const umzug = new Umzug({
+    logging: log,
+    migrations: {
+      path: migrationsPath,
+      pattern: /\.sql$/,
+      customResolver: path => ({
+        up: () => slonik.query(sql`${sql.raw(readFileSync(path, 'utf8'))}`),
+        down: async () => {
+          const downPath = join(dirname(path), 'down', basename(path))
+          if (existsSync(downPath)) {
+            await slonik.query(sql`${sql.raw(readFileSync(downPath, 'utf8'))}`)
+          }
+        },
+      }),
+    },
+    storage: {
+      async executed() {
+        await createMigrationTable()
+        return slonik
+          .any(sql`select name, hash from migration`)
+          .then(migrations => {
+            log('migrations executed:', migrations)
+            return migrations
+          })
+          .then(migrations => migrations.map(r => {
+            const name = r.name as string
+            if (r.hash !== hash(name)) {
+              console.error(
+                `hash in migration table didn't match content on disk.`,
+                `did you try to change a migration after it had been run?`,
+                {migration: r.name, dbHash: r.hash, diskHash: hash(name)}
+              )
+            }
+            return name
+          }))
+      },
+      async logMigration(name: string) {
+        await createMigrationTable()
+        await slonik
+          .query(sql`insert into migration(name, hash) values (${name}, ${hash(name)})`)
+      },
+      async unlogMigration(name: string) {
+        await createMigrationTable()
+        await slonik
+          .query(sql`delete from migration where name = ${name}`)
+      }
+    },
+  })
+
+  const migrator = {
+    up: (name?: string) => umzug.up(name),
+    down: (name?: string) => umzug.down(name),
+    create: (name: string) => {
+      const timestamp = new Date().toISOString().replace(/\W/g, '-').replace(/-\d\d-\d\d\dZ/, '')
+      const sqlFileName = `${timestamp}.${name}.sql`
+      const downDir = join(migrationsPath, 'down')
+      if (!existsSync(downDir)) {
+        mkdirSync(downDir, {recursive: true})
+      }
+      writeFileSync(join(migrationsPath, sqlFileName), `--${name} (up)\n`, 'utf8')
+      writeFileSync(join(downDir, sqlFileName), `--${name} (down)\n`, 'utf8')
+    }
+  }
+  const [command, name] = args
+  if (require.main === mainModule && command in migrator) {
+    migrator[command as keyof typeof migrator](name)
+  }
+
+  return migrator
+}
