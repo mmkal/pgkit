ea0578dda14d28d4d8f54a5848d159859808f5fa
mmkal <misha.kaletsky@gmail.com>
2020-03-23 17:55:24 -0400

    test: test deduping

--- packages/typegen/test/generated/main/FooSubset.ts
+++ packages/typegen/test/generated/main/FooSubset.ts
@@ -26,6 +26,7 @@ export interface FooSubset_QueryTypeMap {
   [`select a from foo`]: FooSubset_AllTypes[0]
   [`select a, b from foo`]: FooSubset_AllTypes[1]
   [`select a, b, c from foo`]: FooSubset_AllTypes[2]
+  [`select a, b, c from foo where 1 = 1`]: FooSubset_AllTypes[2]
 }
 
 export type FooSubset_UnionType = FooSubset_QueryTypeMap[keyof FooSubset_QueryTypeMap]
@@ -35,4 +36,4 @@ export type FooSubset = {
 }
 export const FooSubset = {} as FooSubset
 
-export const FooSubset_meta_v0 = [{"properties":[{"name":"a","value":"string","description":"pg_type.typname: text"}],"description":"select a from foo"},{"properties":[{"name":"a","value":"string","description":"pg_type.typname: text"},{"name":"b","value":"boolean","description":"pg_type.typname: bool"}],"description":"select a, b from foo"},{"properties":[{"name":"a","value":"string","description":"pg_type.typname: text"},{"name":"b","value":"boolean","description":"pg_type.typname: bool"},{"name":"c","value":"string[]","description":"pg_type.typname: _text"}],"description":"select a, b, c from foo"}]
+export const FooSubset_meta_v0 = [{"properties":[{"name":"a","value":"string","description":"pg_type.typname: text"}],"description":"select a from foo"},{"properties":[{"name":"a","value":"string","description":"pg_type.typname: text"},{"name":"b","value":"boolean","description":"pg_type.typname: bool"}],"description":"select a, b from foo"},{"properties":[{"name":"a","value":"string","description":"pg_type.typname: text"},{"name":"b","value":"boolean","description":"pg_type.typname: bool"},{"name":"c","value":"string[]","description":"pg_type.typname: _text"}],"description":"select a, b, c from foo"},{"properties":[{"name":"a","value":"string","description":"pg_type.typname: text"},{"name":"b","value":"boolean","description":"pg_type.typname: bool"},{"name":"c","value":"string[]","description":"pg_type.typname: _text"}],"description":"select a, b, c from foo where 1 = 1"}]
