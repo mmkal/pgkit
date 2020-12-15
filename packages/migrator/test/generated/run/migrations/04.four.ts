4b96a24a3fc425c3ce9422bb80419612ba08de88
mmkal <misha.kaletsky@gmail.com>
2020-12-15 13:27:31 +0000

    update generated

--- packages/migrator/test/generated/run/migrations/04.four.ts
+++ packages/migrator/test/generated/run/migrations/04.four.ts
@@ -1,4 +1,4 @@
-import {Migration} from '../..'
+import {Migration} from '../../../..'
 
 export const up: Migration = ({slonik, sql}) => slonik.query(sql`create table migration_test_4(id int)`)
 export const down: Migration = ({slonik, sql}) => slonik.query(sql`drop table migration_test_4`)
