54d88468a7a3a13cd54a241301c2e5619141371b
mmkal <misha.kaletsky@gmail.com>
2020-12-18 19:18:20 +0000

    sampleTypeValues

--- packages/typegen/src/gdesc/slonik/index.ts
+++ packages/typegen/src/gdesc/slonik/index.ts
@@ -1,7 +1,7 @@
 import {TypeScriptTypeParser} from '../types'
 import * as slonik from 'slonik'
 
-const matchers: Array<[type: string, test: (value: unknown) => boolean]> = [
+const jsValueMatchers: Array<[type: string, test: (value: unknown) => boolean]> = [
   ['number', val => typeof val === 'number'],
   ['string', val => typeof val === 'string'],
   ['boolean', val => typeof val === 'boolean'],
@@ -9,9 +9,32 @@ const matchers: Array<[type: string, test: (value: unknown) => boolean]> = [
   ['Date', val => val instanceof Date],
 ]
 
-export const inferTypeParserTypeScript = (tp: slonik.TypeParserType<any>, sampleInput = '') => {
-  const sample = tp.parse(sampleInput)
-  const match = matchers.find(m => m[1](sample))
+/**
+ * Mapping from pg_type.typname to a valid sample value
+ */
+// todo: explicitly test to see how these all come back by default from pg/slonik
+// e.g. by doing sql`select ${sampleValue}::${sampleValueType}` somehow
+export const sampleTypeValues: Record<string, any> = {
+  int8: 0,
+  date: '2000-01-01',
+  interval: '1 hour',
+  numeric: 0,
+  timestamp: '2000-01-01',
+  timestamptz: '2000-01-01',
+  varchar: '',
+  text: '',
+  smallint: 0,
+  integer: 0,
+  bigint: 0,
+  decimal: 0,
+  serial: 0,
+  bigserial: 0,
+  money: '$0.00',
+}
+
+export const inferTypeParserTypeScript = (tp: slonik.TypeParserType<any>, defaultSampleInput = '') => {
+  const sample = tp.parse(sampleTypeValues[tp.name] || defaultSampleInput)
+  const match = jsValueMatchers.find(m => m[1](sample))
   return match?.[0] || `unknown /* ${tp.name} */`
 }
 
