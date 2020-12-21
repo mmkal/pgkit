fd84fc6686ceff24a8315e1b5fd1359dae6a10c6
mmkal <misha.kaletsky@gmail.com>
2020-12-21 23:51:15 +0000

    get columns and tables, if possible

--- packages/typegen/src/gdesc/parse/index.ts
+++ packages/typegen/src/gdesc/parse/index.ts
@@ -20,84 +20,125 @@ export const parse = (sql: string): any => {
     // todo: don't throw (find out what slonik/other clients do here?)
     throw new Error(`Can't parse query ${sql}; it has ${statements.length} statements.`)
   }
-  const ast = statements[0]
-  // if (Math.random()) return ast
-  const cols = ast.type === 'select' ? ast.columns! : ast.type === 'insert' ? ast.returning : []
-  const colExpressions: Array<{table?: string; name: string}> = cols!
-    .map(c => c.expr)
-    .map(c =>
-      match(c)
-        // .target<{table?: string; name: string}>()
-        .case(
-          // e.g. `select oid from pg_type`
-          {type: 'ref'} as const,
-          e => e,
-        )
-        .case(
-          // e.g. `select oid::regtype from pg_type`
-          {type: 'cast', operand: {type: 'ref'}} as const,
-          e => e.operand,
-        )
-        .default(() => ({table: undefined, name: '???'}))
-        .get(),
-    )
-    .map(c => ({table: c.table, name: c.name}))
+  let ast = statements[0]
 
-  return {
-    sql,
-    suggestedTag: match(ast)
-      .case({type: 'select'} as const, q =>
-        (q.from || []).map(f =>
+  if ((ast.type === 'update' || ast.type === 'insert') && ast.returning) {
+    ast = {
+      type: 'select',
+      from: [
+        {
+          type: 'table',
+          name: ast.type === 'update' ? ast.table.name : ast.into.name,
+        },
+      ],
+      columns: ast.returning.map(r => ({
+        expr: r.expr,
+      })),
+    }
+  }
+
+  if (ast.type === 'select') {
+    return {
+      tables: ast.from
+        ?.map(f =>
           match(f)
-            .case({name: String}, fr => fr.name)
-            .default(() => 'unknown')
+            .case({type: 'table'} as const, t => t.name)
+            .default(f => f.alias)
             .get(),
-        ),
-      )
-      .default(() => ['unknown'])
-      .get()
-      ?.join('_'),
-    // ),
-    columns: colExpressions.map(c => ({
-      name: c.name,
-      table: c.table,
-      notNull: false,
-    })),
+        )
+        .filter(Boolean),
+      columns: ast.columns
+        ?.map<string>(
+          c =>
+            c.alias ||
+            match(c.expr)
+              .case({type: 'ref' as const}, e => e.name)
+              .case({type: 'call', function: String} as const, e => e.function)
+              .default(() => '')
+              .get(),
+        )
+        .filter(Boolean),
+    }
   }
-}
 
-export const parse2 = (sql: string): any | Omit<ParsedQuery, 'tag' | 'file'> => {
-  const surveyor = new sqlSurveyor.SQLSurveyor(sqlSurveyor.SQLDialect.PLpgSQL)
-  const ast = surveyor.survey(sql)
-  if (Object.keys(ast.parsedQueries).length !== 1) {
-    // throw 'foo'
-  }
-  const query = ast.getQueryAtLocation(0)
-  if (Math.random()) {
-    // query.get
-    return [
-      query.outputColumns,
-      query.referencedTables, //
-      query.getTableFromAlias('as f'),
-      query,
-    ] as any
-  }
-  return {
-    sql,
-    suggestedTag: lodash
-      .chain(query.outputColumns)
-      .map(c => c.tableName)
-      .uniq()
-      .map(pascalCase)
-      .join('_')
-      .value(),
-    // @ts-expect-error
-    columns: query.outputColumns.map(c => ({
-      table: c.tableName,
-      name: c.columnAlias,
-      notNull: false,
-    })),
-  }
+  return {}
+
+  //   const cols = ast.type === 'select' ? ast.columns! : ast.type === 'insert' ? ast.returning : []
+  //   const colExpressions: Array<{table?: string; name: string}> = cols!
+  //     .map(c => c.expr)
+  //     .map(c =>
+  //       match(c)
+  //         // .target<{table?: string; name: string}>()
+  //         .case(
+  //           // e.g. `select oid from pg_type`
+  //           {type: 'ref'} as const,
+  //           e => e,
+  //         )
+  //         .case(
+  //           // e.g. `select oid::regtype from pg_type`
+  //           {type: 'cast', operand: {type: 'ref'}} as const,
+  //           e => e.operand,
+  //         )
+  //         .default(() => ({table: undefined, name: '???'}))
+  //         .get(),
+  //     )
+  //     .map(c => ({table: c.table, name: c.name}))
+
+  //   return {
+  //     sql,
+  //     suggestedTag: match(ast)
+  //       .case({type: 'select'} as const, q =>
+  //         (q.from || []).map(f =>
+  //           match(f)
+  //             .case({name: String}, fr => fr.name)
+  //             .default(() => 'unknown')
+  //             .get(),
+  //         ),
+  //       )
+  //       .default(() => ['unknown'])
+  //       .get()
+  //       ?.join('_'),
+  //     // ),
+  //     columns: colExpressions.map(c => ({
+  //       name: c.name,
+  //       table: c.table,
+  //       notNull: false,
+  //     })),
+  //   }
+  // }
+
+  // export const parse2 = (sql: string): any | Omit<ParsedQuery, 'tag' | 'file'> => {
+  //   const surveyor = new sqlSurveyor.SQLSurveyor(sqlSurveyor.SQLDialect.PLpgSQL)
+  //   const ast = surveyor.survey(sql)
+  //   if (Object.keys(ast.parsedQueries).length !== 1) {
+  //     // throw 'foo'
+  //   }
+  //   const query = ast.getQueryAtLocation(0)
+  //   if (Math.random()) {
+  //     // query.get
+  //     return [
+  //       query.outputColumns,
+  //       query.referencedTables, //
+  //       query.getTableFromAlias('as f'),
+  //       query,
+  //     ] as any
+  //   }
+  //   return {
+  //     sql,
+  //     suggestedTag: lodash
+  //       .chain(query.outputColumns)
+  //       .map(c => c.tableName)
+  //       .uniq()
+  //       .map(pascalCase)
+  //       .join('_')
+  //       .value(),
+  //     // @ts-expect-error
+  //     columns: query.outputColumns.map(c => ({
+  //       table: c.tableName,
+  //       name: c.columnAlias,
+  //       notNull: false,
+  //     })),
+  //   }
 }
 
 if (require.main === module) {
@@ -105,6 +146,12 @@ if (require.main === module) {
   // console.dir(parse('insert into foo(id) values (1) returning id, date'), {depth: null})
   // console.dir(parse('select pt.typname, foo.bar::regtype from pg_type as pt join foo on pg_type.id = foo.oid'), {depth: null})
   // console.dir(parse('select foo::regtype from foo'), {depth: null})
-  console.dir(parse('select i, j from a join b on 1=1'), {depth: null})
-  console.dir(parse(`select count(*) from foo`), {depth: null})
+  // console.dir(parse('select i, j from a join b on 1=1'), {depth: null})
+  console.log = (x: any) => console.dir(x, {depth: null})
+  console.dir(parse(`select count(*), * from foo where y = null`), {depth: null})
+  console.dir(parse(`select pg_advisory_lock(123), x, y from foo`), {depth: null})
+  console.dir(parse(`insert into foo(id) values (1) returning *`), {depth: null})
+  console.dir(parse(`insert into foo(id) values (1)`), {depth: null})
+  console.dir(parse(`update foo set bar = 'baz' returning *`), {depth: null})
+  console.dir(parse(`select foo.x from foo where y = null`), {depth: null})
 }
