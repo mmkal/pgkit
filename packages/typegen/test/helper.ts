7efd652dbb8c35758a14b08e6d012037c9f515de
mmkal <misha.kaletsky@gmail.com>
2021-01-10 21:57:16 -0500

    Copy over pool helper

--- packages/typegen/test/helper.ts
+++ packages/typegen/test/helper.ts
@@ -1,5 +1,6 @@
+import {createPool, sql, ClientConfigurationInputType} from 'slonik'
 import * as typegen from '../src'
-import {getPoolHelper} from '@slonik/migrator/test/pool-helper'
+import * as path from 'path'
 
 export const baseConnectionURI = `postgresql://postgres:postgres@localhost:5432/postgres`
 export const psqlCommand = `docker-compose exec -T postgres psql`
@@ -25,4 +26,48 @@ export const getHelper = (params: {__filename: string}) => {
   jest.setTimeout(30000)
 
   return {gdescParams, poolHelper, logger}
+}
+
+export const getPoolHelper = (params: {
+  __filename: string
+  baseConnectionURI: string
+  config?: ClientConfigurationInputType
+}) => {
+  const schemaName = path.parse(params.__filename).name.replace(/\W/g, '_')
+  const schemaIdentifier = sql.identifier([schemaName])
+
+  const pool = createPool(params.baseConnectionURI, {
+    idleTimeout: 1,
+    ...params?.config,
+    interceptors: [
+      {
+        afterPoolConnection: async (context, connection) => {
+          await connection.query(sql`set search_path to ${schemaIdentifier}`)
+          return null
+        },
+      },
+      ...(params?.config?.interceptors ?? []),
+    ],
+  })
+
+  // https://github.com/gajus/slonik/issues/63#issuecomment-500889445
+  afterAll(() => new Promise(r => setTimeout(r, 1)))
+
+  beforeEach(async () => {
+    await pool.query(sql`drop schema if exists ${schemaIdentifier} cascade`)
+    await pool.query(sql`create schema ${schemaIdentifier}`)
+  })
+
+  const mockLog = jest.fn()
+  const mockLogger = {
+    debug: mockLog,
+    info: mockLog,
+    warn: mockLog,
+    error: mockLog,
+  }
+
+  /** Get the names from a list of migrations. Useful for light assertions */
+  const names = (migrations: Array<{name: string}>) => migrations.map(m => m.name)
+
+  return {pool, schemaName, schemaIdentifier, mockLogger, names, sql}
 }
