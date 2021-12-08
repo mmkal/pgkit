ae2acb51d881b48590213e5e3909888ccd626de8
Misha Kaletsky <mmkal@users.noreply.github.com>
2021-12-08 16:12:52 +0000

    fix typo

--- packages/migrator/src/templates.ts
+++ packages/migrator/src/templates.ts
@@ -17,12 +17,12 @@ export const down: Migration = async ({context: {connection, sql}}) => {
 `.trimLeft()
 
 export const javascript = `
-/** @type {import('@sloink/migrator').Migration} */
+/** @type {import('@slonik/migrator').Migration} */
 exports.up = async ({context: {connection, sql}}) => {
   await connection.query(sql\`raise 'up migration not implemented'\`)
 }
 
-/** @type {import('@sloink/migrator').Migration} */
+/** @type {import('@slonik/migrator').Migration} */
 exports.down = async ({context: {connection, sql}}) => {
   await connection.query(sql\`raise 'down migration not implemented'\`)
 }
