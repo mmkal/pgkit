98f869d6703ed86ebf6590780fe9309d63a915ba
Misha Kaletsky <mmkal@users.noreply.github.com>
2024-03-21 22:29:02 -0400

    fix readme bad fixes

--- packages/migrator/eslint.config.js
+++ packages/migrator/eslint.config.js
@@ -1,1 +1,9 @@
-module.exports = require('eslint-plugin-mmkal').recommendedFlatConfigs
+module.exports = [
+  ...require('eslint-plugin-mmkal').recommendedFlatConfigs,
+  {
+    files: ['**/*.md/*commented-out.js'],
+    rules: {
+      'prettier/processed': 'off', // mmkal filter out rules other than codegen on the codegen-preprocessed file
+    },
+  },
+]
