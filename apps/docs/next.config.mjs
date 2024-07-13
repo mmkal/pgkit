9c793f1b157d0e262a1226e7a5ffba4931264f25
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2024-07-13 07:42:47 -0400

    Update next.config.mjs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -4,7 +4,6 @@ const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
-
 export default withNextra({
   eslint: {ignoreDuringBuilds: true},
   typescript: {ignoreBuildErrors: true},
