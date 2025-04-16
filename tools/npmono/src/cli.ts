b76a51ce4019e8c8a1255f8d4f5f88b5b2f13e44
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-04-16 16:11:52 -0400

    prep-link (i think this is dumb and will revert)

--- tools/npmono/src/cli.ts
+++ tools/npmono/src/cli.ts
@@ -1,3 +1,6 @@
+import {execa} from '@rebundled/execa'
+import {readdir} from 'fs/promises'
+import * as path from 'path'
 import * as trpcCli from 'trpc-cli'
 import {z} from 'trpc-cli'
 import {releaseNotes, ReleaseNotesInput, publish, PublishInput, PrebuiltInput, publishPrebuilt} from './publish'
@@ -24,6 +27,38 @@ const router = t.router({
     ) //
     .mutation(async ({input}) => {
       return releaseNotes(input)
+    }),
+
+  prepLink: t.procedure
+    .meta({
+      description:
+        'preps package(s) for local linking. useful if you want to try out your packages in another project. prints a link command to stdout',
+    })
+    .input(
+      z.object({
+        prebuilt: z.string().describe('prebuilt folder').optional(),
+        package: z.string().describe('package name to link. if ommitted all packages will printed').optional(),
+        install: z.string().default('npm install --include prod'),
+      }),
+    )
+    .mutation(async ({input: options}) => {
+      if (!options.prebuilt) {
+        const dateVersion =
+          new Date().toISOString().split('T')[0].split('-').join('.').replaceAll('.0', '.') + -Date.now()
+        const ctx = await publish({
+          version: dateVersion,
+          publish: false,
+        })
+        options.prebuilt = ctx.tempDir
+      }
+
+      await execa('sh', ['-c', options.install])
+      console.log(`run the following to link package(s) locally:`)
+
+      const names = await readdir(options.prebuilt)
+      for (const name of names.filter(n => !options.package || n.replace(/^\d+\./, '') === options.package)) {
+        console.log(`npx link ${path.join(options.prebuilt, name, 'right/package')}`)
+      }
     }),
 })
 
