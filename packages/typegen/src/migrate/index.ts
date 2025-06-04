5877d7a0c9e29e425cfebae3d5337fc1bd6fb1da
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-06-04 12:38:59 -0400

    avoid void

--- packages/typegen/src/migrate/index.ts
+++ packages/typegen/src/migrate/index.ts
@@ -3,9 +3,9 @@ import {migrate080} from './lte0.8.0'
 
 export const migrateLegacyCode =
   (from: NonNullable<Options['migrate']>) => async (params: {files: string[]; logger: Logger}) => {
-    const handlers: Record<typeof from, (p: typeof params) => void> = {
+    const handlers: Record<typeof from, (p: typeof params) => unknown> = {
       '<=0.8.0': migrate080,
     }
 
-    return void handlers[from](params)
+    return handlers[from](params)
   }
