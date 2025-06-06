4619184f086207b8a07033f9864db1e55a87d1e9
Misha Kaletsky <mmkal@users.noreply.github.com>
2025-06-05 20:25:44 -0400

    dumb logs

--- packages/migra/test/python-parity.test.ts
+++ packages/migra/test/python-parity.test.ts
@@ -25,19 +25,26 @@ test.each(fixtures)(
   '%j migra fixture',
   async ({name, args, ...fixture}) => {
     expect(name).toMatch(/^[\d_a-z]+$/)
+    console.log(1001)
     const [a, b] = await fixture.setup(admin)
+    console.log(1002)
 
     const {execa} = await import('execa')
+    console.log(1003)
 
     const expected = await execa('migra', [a, b, ...fixture.cliArgs()], {
       cwd: process.cwd(),
       env: process.env,
       reject: false, // migra exits with a non-zero code when there are changes
     }).then(p => format(p.stderr).trim() || format(p.stdout))
+    console.log(1004)
     const migra = await runMigra(a, b, args())
+    console.log(1005)
     const actual = format(migra.sql)
+    console.log(1006)
 
     expect(actual).toEqual(expected)
+    console.log(1007)
   },
   10_000,
 )
