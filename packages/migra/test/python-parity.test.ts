2104cc30e1ce07401461961a2645383277435caf
Philip Andrew Wee De Wang <41712275+PhilipWee@users.noreply.github.com>
2025-06-11 11:53:48 +0800

    wip: added new folder for additional features on top of original

--- packages/migra/test/python-parity.test.ts
+++ packages/migra/test/python-parity.test.ts
@@ -1,7 +1,8 @@
 import {createPool} from '@pgkit/client'
+import * as path from 'path'
 import {beforeAll, expect, test} from 'vitest'
 import {run as runMigra} from '../src/command'
-import {getFixtures, format} from './fixtures'
+import {format, getFixtures} from './fixtures'
 import {runOriginalMigra} from './run-original-migra'
 
 export let admin: Awaited<ReturnType<typeof createPool>>
@@ -10,7 +11,11 @@ beforeAll(async () => {
   admin = createPool('postgresql://postgres:postgres@localhost:5432/postgres')
 })
 
-const fixtures = getFixtures('python_parity').map(f => [f] as const)
+const originalFixturesDir = path.join(__dirname, 'FIXTURES')
+const originalFixtures = getFixtures('python_parity', originalFixturesDir).map(f => [f] as const)
+
+const newFixturesDir = path.join(__dirname, 'NEW_FIXTURES')
+const newFixtures = getFixtures('new_features', newFixturesDir).map(f => [f] as const)
 
 test('python migra CLI is installed and on PATH', async () => {
   const {stdout, stderr} = await runOriginalMigra('--help')
@@ -18,8 +23,8 @@ test('python migra CLI is installed and on PATH', async () => {
   expect(stdout).toContain('Generate a database migration.')
 }, 60_000) //Give extra time to pull docker image if necessary
 
-test.each(fixtures)(
-  '%j migra fixture',
+test.each(originalFixtures)(
+  '%j python parity migra fixture',
   async ({name, args, ...fixture}) => {
     expect(name).toMatch(/^[\d_a-z]+$/)
     const [a, b] = await fixture.setup(admin)
@@ -27,6 +32,20 @@ test.each(fixtures)(
     const rawOutput = await runOriginalMigra(a, b, ...fixture.cliArgs())
     const expected = format(rawOutput.stderr).trim() || format(rawOutput.stdout)
     // const expected = format(fixture.getExpected())
+    const migra = await runMigra(a, b, args())
+    const actual = format(migra.sql)
+
+    expect(actual).toEqual(expected)
+  },
+  10_000,
+)
+
+test.each(newFixtures)(
+  '%j new feature migra fixture',
+  async ({name, args, ...fixture}) => {
+    expect(name).toMatch(/^[\d_a-z]+$/)
+    const [a, b] = await fixture.setup(admin)
+    const expected = format(fixture.getExpected())
     const migra = await runMigra(a, b, args())
     const actual = format(migra.sql)
 
